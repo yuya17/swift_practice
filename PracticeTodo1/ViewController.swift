890bccb253d69370093a686d8d4400c2f52f4a74
yuya17 <yuya.noza17@gmail.com>
2018-05-25 01:16:33 +0900

    Initial Commit

---
+++ PracticeTodo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeTodo1
+//
+//  Created by 野崎祐矢 on 2018/05/25.
+//  Copyright © 2018年 野崎祐矢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
